571fcc0e30b6537227f880a9c52bf76770b734f9
zeze.li <lizeze.boy@gmail.com>
2021-07-06 09:15:24 +0800

    first commit

---
+++ src/main/java/com/github/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package com.github.utils;
+
+/**
+ * @author lzz
+ * @create 2021-07-05 17:26
+ */
+public class StringUtils {
+
+    public static boolean isEmpty(String str) {
+        return str == null;
+    }
+
+}
